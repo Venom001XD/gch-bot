42a39742117d3e709340d008720b79be1ab10754
Yesyesyesstark <115603133+Yesyesyesstark@users.noreply.github.com>
2022-10-21 14:40:54 +0530

    Update config.py

--- Himawari/config.py
+++ Himawari/config.py
@@ -1,28 +1,5 @@
- """
-MIT License
 
-Copyright (c) 2022 Arsh
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 import json
 import os
 
@@ -41,10 +18,10 @@ class Config(object):
     TEMP_DOWNLOAD_DIRECTORY="./"
     DEL_CMDS=False
     BAN_STICKER="kans" 
-    CERT_PATH=""
+    CERT_PATH="True"
     PORT=8443
     WORKERS=8
-    LOAD=""
+    LOAD="True"
     NO_LOAD="translation"
     MONGO_DB="Himawari"
     WEBHOOK=False

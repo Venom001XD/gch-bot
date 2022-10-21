80f476d4849887ac2e896e029118fc9f7e7eb7ff
XoRishu <115208147+XoRishu@users.noreply.github.com>
2022-10-22 01:00:38 +0530

    Update feds.py

--- Himawari/modules/feds.py
+++ Himawari/modules/feds.py
@@ -2120,7 +2120,7 @@ def fed_user_help(update: Update, context: CallbackContext):
  • `/frules`*:* See Federation regulations\n""",
         parse_mode=ParseMode.MARKDOWN)
 		   
-__mod_name__ = "Feds"
+__mod_name__ = "𝙵ᴇᴅs"
 
 __help__ = """
 Everything is fun, until a spammer starts entering your group, and you have to block it. Then you need to start banning more, and more, and it hurts.

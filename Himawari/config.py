651967e8e8b18041d11f3006e37dc802957eb931
Yesyesyesstark <115603133+Yesyesyesstark@users.noreply.github.com>
2022-10-23 04:33:20 +0530

    Update config.py

--- Himawari/config.py
+++ Himawari/config.py
@@ -62,7 +62,7 @@ class Config(object):
     DB_URL2="mongodb+srv://ssid143:liyaxlambert*143@cluster0.pdabrye.mongodb.net/?retryWrites=true&w=majority" #mongo db (not necessary)
     DB_URL="postgres://fdgyqfrh:47TwHvQzYU5efrUT5uw2WMsS9mIe_WTU@lucky.db.elephantsql.com/fdgyqfrh" #postgres sql database link
     REDIS_URL="redis://default:GDLSeElPaIlt9sCXKsWf@containers-us-west-107.railway.app:5671" #redis database url from redislabs.com
-    TOKEN="5518786755:AAE-S6aKhhuZ34E5H7Ay6xVvltED7mAiMdE" #bot token from @BotFather
+    TOKEN="5518786755:AAGcId98RAvswtmW-w31s4DbTm7H_m7jAKk" #bot token from @BotFather
     DEV_USERS=[5306064258] #developers id
     DRAGONS=[9656] #sudo users id
     DEMONS=[1909] #support user ids

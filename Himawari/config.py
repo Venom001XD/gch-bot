d47dea9bc30d7608c1aba4530934a3dc7689ed21
Yesyesyesstark <115603133+Yesyesyesstark@users.noreply.github.com>
2022-10-23 04:30:05 +0530

    Update config.py

--- Himawari/config.py
+++ Himawari/config.py
@@ -1,5 +1,28 @@
+"""
+MIT License
 
+Copyright (c) 2022 Arsh
 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 import json
 import os
 
@@ -25,45 +48,44 @@ class Config(object):
     NO_LOAD="translation"
     MONGO_DB="Himawari"
     WEBHOOK=False
-    BOT_API_URL="https://api.telegram.org/PowerUltraXBot"
+    BOT_API_URL="https://api.telegram.org/bot"
 
     #you can change these 
     INFOPIC=True #picture while doing /info
     STRICT_GBAN=True
-    API_ID=16228540 ##api id from my.telegram.org
-    APP_ID=16228540 #same as API_ID
-    API_HASH="57a8fac91b598c8c2e7fdde3c8407020" ##api hash from my.telegram.org
-    APP_HASH="57a8fac91b598c8c2e7fdde3c8407020" #same as API_HASH
+    API_ID=18126477 ##api id from my.telegram.org
+    APP_ID=18126477 #same as API_ID
+    API_HASH="e713e1187e6d140bc1a2119f603abdb6" ##api hash from my.telegram.org
+    APP_HASH="e713e1187e6d140bc1a2119f603abdb6" #same as API_HASH
     BL_CHATS=[1] #chats to be blacklisted
     MONGO_DB_URL="mongodb+srv://ssid143:liyaxlambert*143@cluster0.pdabrye.mongodb.net/?retryWrites=true&w=majority" ##mongo database link (necessary)
-    DB_URL2="mongodb+srv://meowhisswswuj7.mongodb.net/?retryWrites=true&w=majority" #mongo db (not necessary)
+    DB_URL2="mongodb+srv://ssid143:liyaxlambert*143@cluster0.pdabrye.mongodb.net/?retryWrites=true&w=majority" #mongo db (not necessary)
     DB_URL="postgres://fdgyqfrh:47TwHvQzYU5efrUT5uw2WMsS9mIe_WTU@lucky.db.elephantsql.com/fdgyqfrh" #postgres sql database link
     REDIS_URL="redis://default:GDLSeElPaIlt9sCXKsWf@containers-us-west-107.railway.app:5671" #redis database url from redislabs.com
     TOKEN="5518786755:AAE-S6aKhhuZ34E5H7Ay6xVvltED7mAiMdE" #bot token from @BotFather
-    DEV_USERS=[5556308886] #developers id
+    DEV_USERS=[5306064258] #developers id
     DRAGONS=[9656] #sudo users id
     DEMONS=[1909] #support user ids
     TIGERS=[1] #commas for multiple ids
     WOLVES=[2112, 1212] #commas for multiple ids 
-    DONATION_LINK="https://t.me/PowerSupportGroup" #u can change with yours
+    DONATION_LINK="https://www.paypal.me/PaulSonOfLars" #u can change with yours
     EVENT_LOGS=-100159 #channel id for gban logs
     JOIN_LOGGER=-1001523  #log channel/group id
-    OWNER_ID=1937701729 #owner id in integer
+    OWNER_ID=5306064258 #owner id in integer
     ERROR_LOGS=-1001 #support group id
-    BOT_NAME="Power" #your bot name
-    ARQ_API_KEY="EIFSEZ-FKVDZN-WHNGHO-DIZNLM-ARQ" #ARQ api key from @ARQRobot
+    BOT_NAME="Himawari" #your bot name
+    ARQ_API_KEY="SLSFXSsdUXNSMH-ARQ" #ARQ api key from @ARQRobot
     ARQ_API_URL="arq.hamker.dev" #arq link
-    SUPPORT_CHAT="PowerSupportGroup" #support group username without @
-    OWNER_USERNAME="SIXTH_H0KAGE" #owner username without @
-    UPDATES_CHANNEL="PowerBotUpdates" #Updates/News Channel username without @
-    BOT_USERNAME="PowerUltraXBot" #bot username without @
-    REM_BG_API_KEY="LSdLgCceYz8vNqFgJVzrkDgR" #not necessary
-    GENIUS_API_TOKEN="28jwoKAkskaSjsnsksAjnwjUJwj" # api token from genius.com (not necessary)
+    SUPPORT_CHAT="Ignidsn" #support group username without @
+    OWNER_USERNAME="arsshh" #owner username without @
+    UPDATES_CHANNEL="Igsd" #Updates/News Channel username without @
+    BOT_USERNAME="Himawari_Robot" #bot username without @
+    REM_BG_API_KEY="K2dsdsYma6cZx" #not necessary
+    GENIUS_API_TOKEN="e-8UdRQNrIssPyM" # api token from genius.com (not necessary)
     TIME_API_KEY="QLLLDV7SWFD3" #not necessary
-    SPAMWATCH_API="IDFrVHvi~xbI5VKhZWfCg7uv5Dqf_kwq_HqxTJkVfUaZLx2tDY09w901XI2t6CDH" #spamwatch api token from @SpamWatchBot
-    WALL_API="2455acab48f3a935a8e703e54e26d121" #wall api (not necessary)
-    BOT_ID = 1937701729
-
+    SPAMWATCH_API="J968E_20LgxrKjsdN24cqYtD~gNRTbU" #spamwatch api token from @SpamWatchBot
+    WALL_API="6950f5ds6a3" #wall api (not necessary)
+    BOT_ID = 5556308886
 
 class Production(Config):
     LOGGER=True

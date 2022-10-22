6d4a40572737ea77b1f21a97c6d788aa2ed2cd37
XoRishu <115208147+XoRishu@users.noreply.github.com>
2022-10-22 22:50:16 +0530

    Update misc.py

--- Himawari/modules/misc.py
+++ Himawari/modules/misc.py
@@ -394,7 +394,7 @@ ECHO_HANDLER = DisableAbleCommandHandler("echo", echo, filters=Filters.chat_type
 
 dispatcher.add_handler(ECHO_HANDLER)
 
-__mod_name__ = "Extras"
+__mod_name__ = "𝙴xᴛʀᴀs"
 __command_list__ = ["id", "echo"]
 __handlers__ = [
     ECHO_HANDLER

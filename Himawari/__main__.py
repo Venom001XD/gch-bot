8ebd4869cdb7a3d91f773c16c3b22387ec76f689
Yesyesyesstark <115603133+Yesyesyesstark@users.noreply.github.com>
2022-10-22 19:18:49 +0530

    Update __main__.py

--- Himawari/__main__.py
+++ Himawari/__main__.py
@@ -979,5 +979,5 @@ def main():
 if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
-    pbot.start()
+    pgram.start()
     main()
